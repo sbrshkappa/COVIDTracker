98629c17ba434d4bd105e06df737363f2b98ff9c
Sabareesh Kappagantu <sbrsh@sabareeshs-imac.lan>
2020-05-30 09:19:03 -0700

    Updates

---
+++ precache-manifest.981abfd52f571e1d1b056c6db3bbf460.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb133fc506eeb9cda027ad067baa23c",
+    "url": "/COVIDTracker/index.html"
+  },
+  {
+    "revision": "23fb2fdc6567867181c0",
+    "url": "/COVIDTracker/static/css/main.5ae4deb1.chunk.css"
+  },
+  {
+    "revision": "b853e13d9aa5422da25d",
+    "url": "/COVIDTracker/static/js/2.181dde70.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVIDTracker/static/js/2.181dde70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fb2fdc6567867181c0",
+    "url": "/COVIDTracker/static/js/main.9d1fdf3a.chunk.js"
+  },
+  {
+    "revision": "f4fa1bf202a4e097576d",
+    "url": "/COVIDTracker/static/js/runtime-main.2cbd62f3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVIDTracker/static/media/image.d7265326.png"
+  }
+]);
